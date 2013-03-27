c04a6766d43b58b6033de792cd123bfa6525cda4
Andrew Starovoyt <astarovoyt@naumen.ru>
2013-03-27 23:53:40 +0600

    Added RopeNode desc

--- classic-rope/src/ru/usu/data/structure/rope/Rope.java
+++ classic-rope/src/ru/usu/data/structure/rope/Rope.java
@@ -1,10 +1,18 @@
 package ru.usu.data.structure.rope;
 
+import java.util.List;
+
 /**
+ * Rope interface
+ * 
  * @author astarovoyt
  *
  */
 public interface Rope extends CharSequence 
 {
-
+	public List<Rope> split(int index);
+	
+	public Rope merge(Rope left, Rope right);
+	
+	public void append(String string);
 }
--- classic-rope/src/ru/usu/data/structure/rope/RopeImpl.java
+++ classic-rope/src/ru/usu/data/structure/rope/RopeImpl.java
@@ -1,5 +1,7 @@
 package ru.usu.data.structure.rope;
 
+import java.util.List;
+
 /**
  * Rope implementation
  * 
@@ -8,6 +10,22 @@ package ru.usu.data.structure.rope;
  */
 public class RopeImpl implements Rope 
 {
+	public static class RopeNode
+	{
+		RopeNode left;
+		RopeNode right;
+		String value;
+		int influence;
+		
+		public boolean isLeaf()
+		{
+			return null == left && null == right;
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private RopeNode root;
+	
 	@Override
 	public int length() {
 		return 0;
@@ -21,5 +39,20 @@ public class RopeImpl implements Rope
 	@Override
 	public CharSequence subSequence(int start, int end) {
 		return null;
+	}
+
+	@Override
+	public List<Rope> split(int index) {
+		return null;
+	}
+
+	@Override
+	public Rope merge(Rope left, Rope right) {
+		return null;
+	}
+
+	@Override
+	public void append(String string) {
+		
 	}
 }
