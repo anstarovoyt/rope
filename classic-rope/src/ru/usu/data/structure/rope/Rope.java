90e6a638364ee9d70cc9e3684f4ddae8660f644d
Andrew Starovoyt <astarovoyt@naumen.ru>
2013-03-27 23:35:40 +0600

    Added a new project classic-rope

---
+++ classic-rope/src/ru/usu/data/structure/rope/Rope.java
@@ -0,0 +1,10 @@
+package ru.usu.data.structure.rope;
+
+/**
+ * @author astarovoyt
+ *
+ */
+public interface Rope extends CharSequence 
+{
+
+}
---
+++ classic-rope/src/ru/usu/data/structure/rope/RopeImpl.java
@@ -0,0 +1,25 @@
+package ru.usu.data.structure.rope;
+
+/**
+ * Rope implementation
+ * 
+ * @author astarovoyt
+ *
+ */
+public class RopeImpl implements Rope 
+{
+	@Override
+	public int length() {
+		return 0;
+	}
+
+	@Override
+	public char charAt(int index) {
+		return 0;
+	}
+
+	@Override
+	public CharSequence subSequence(int start, int end) {
+		return null;
+	}
+}
